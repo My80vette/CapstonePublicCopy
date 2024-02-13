5f3624d20152448dcd38d528f6397cdfa33ab45a
Stone Mitchell <stone.mitchell.45@gmail.com>
2024-02-13 10:59:48 -0700

    Added print statement

---
+++ streamlit/smeui.py
@@ -0,0 +1,22 @@
+import streamlit as st
+from streamlit_chatbox import *
+
+chat_box = ChatBox()
+
+# tabs relocated to sidebar (nonfunctional currently)
+with st.sidebar:
+    st.title('Mars Ingenuity SME')
+    st.button('Chat', key='chatTab')
+    st.button('Chat History', key='chatHistoryTab')
+
+# simple chat box structure
+chat_box.init_session()
+chat_box.output_messages()
+
+if query := st.chat_input('input your question here'):
+    chat_box.user_say(query)
+    chat_box.ai_say('you said: ' + query)
+
+print("PHOBOS")
+# working on tempurature slider
+# temperature = st.slider('Response Temperature', 0, 100, 50)
