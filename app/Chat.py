baa63d0d40d4e22198e84a6bf01290176a5f2d2c
Stone Mitchell <stone.mitchell.45@gmail.com>
2024-02-15 20:02:34 -0700

    embedding seemingly functional, working on ai search access errors

--- app/Chat.py
+++ app/Chat.py
@@ -2,8 +2,9 @@ from pathlib import Path
 import streamlit as st
 from streamlit_chatbox import *
 from loguru import logger
-import os
+import requests
 from openai import AzureOpenAI
+import json
 
 
 # app title on sidebar
@@ -45,57 +46,61 @@ chat_box = ChatBox()
 chat_box.init_session()
 chat_box.output_messages()
 if query := st.chat_input("input your question here", key="chatBox"):
+
+    # log input
     chat_box.user_say(query)
     logger.info("User sent message: " + query)
 
-    # embedding
-    # api_key = "2f8c4fc6fba44228b5a9a268cc579fe5"
-    # endpoint = "https://ingenuityai.openai.azure.com"
-    # client = OpenAI(api_key=api_key, base_url=endpoint)
-
-    # embeddings = client.embeddings.create(
-    # model="text-embedding-ada-002",
-    # input=query,
-    # encoding_format="float"
-    # )
-
-    # # get search results
-    # search_request = {"embedding": embeddings}
-    # search_url = "https://ingenuity-ai-search.search.windows.net/indexes/vector-1707238357310/docs"
-    # search_results = requests.post(search_url, json=search_request)
-    # results = search_results.json()["value"]
-    # docs = [r["content"] for r in results[:5]]
-
-    # # AI API
-    # prompt = (
-    #     f"Relevant documents: {docs}. Based on these, answer the user query: {query}"
-    # )
-
-    # input_data = {
-    # "language": "en",
-    # "text": query,
-    # }
-    # # Send the request to the Azure OpenAI API
-    # response = client.predict(input_data)
-
-    # response = openai.Completion.create(
-    #     engine="gpt-35-turbo:1106", prompt=prompt, max_tokens=1000
-    # )
-    # gpt_response = response.choices[0].text
-
+    # init client
     client = AzureOpenAI(
         api_key="2f8c4fc6fba44228b5a9a268cc579fe5",
         api_version="2023-07-01-preview",
         azure_endpoint="https://ingenuityai.openai.azure.com/",
     )
+    deployment_name = "ingenuityGPT"
 
-    deployment_name = "ingenuityGPT"  # This will correspond to the custom name you chose for your deployment when you deployed a model. Use a gpt-35-turbo-instruct deployment.
+    # embedding
+    embeddings = client.embeddings.create(
+        model="ingenuityEmbedder",
+        input=query,
+        encoding_format="float"
+    )
 
-    # Send a completion call to generate an answer
+    # get search results
+    endpoint = "https://ingenuity-ai-search.search.windows.net/"
+    index_name = "vector-1707238357310"
+    api_version = "2020-06-30"
+    api_key = "nmnRajq7Ydh4epVjBkBwyRvfrvWDCfjPf7Amf4bRm6AzSeCqIxtX"
+    search_url = f"{endpoint}indexes/{index_name}/docs/search?api-version={api_version}"
+    headers = {
+        "Content-Type": "application/json",
+        "api-key": api_key
+    }
+    params = {
+        "search": query
+        # "select": "fields_to_return"
+    }
+    searchResponse = requests.get(search_url, headers=headers, params=params)
+    if searchResponse.status_code == 200:
+        search_results = searchResponse.json()
+        st.sidebar.write(search_results)
+    else:
+        st.sidebar.write("Failed to retrieve search results:", searchResponse.text)
+        st.sidebar.write(searchResponse.status_code)
+    # search_results = requests.post(search_url, json=embeddings.data[0].embedding)
+    # docs = search_results
+
+    # call AI API
+    prompt = (
+        # f"Relevant documents: {docs}. Based on these, answer the user query: {query}"
+        query
+    )
     response = client.chat.completions.create(
         model=deployment_name,
-        messages=[{"role": "system", "content": query}],
+        messages=[{"role": "system", "content": prompt}],
     )
+
+    # display response
     chat_box.ai_say(response.choices[0].message.content)
 
 # init page
@@ -108,6 +113,3 @@ if "chatInit" not in st.session_state:
     st.session_state["chatInit"] = True
     logger.remove()
     initlogger()
-
-# run the app
-# st.run()
