1a8f0364a05c9049c668f76d9447326e443d218e
Nicholas Capriotti <CAPRIOTN@my.erau.edu>
2024-04-03 16:15:44 -0700

    Error logging fixed and tested, no more red boxes

--- app/log_setup.py
+++ app/log_setup.py
@@ -1,6 +1,9 @@
 import logging
+import os
 from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 from datetime import datetime
+import random
+import string
 
 # Set up credentials from the $logs container
 #Connection String from the Ingenuitycontextstorage -> access keys
@@ -24,9 +27,19 @@ file_handler.setFormatter(formatter)
 # Add the file handler to the logger
 logger.addHandler(file_handler)
 
-# Upload the error log to Azure Blob Storage
-def upload_error_log(container_client):
-    with open('error.log', 'rb') as file:
-        blob_client = container_client.get_blob_client(f'error_{datetime.now().strftime("%Y%m%d%H%M%S")}.log')
-        blob_client.upload_blob(file)
+def upload_error_log(logs_container_client, error_message):
+    # Generate a unique file name using the timestamp and a random string
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    random_string = ''.join(random.choices(string.ascii_letters + string.digits, k=3))
+    file_name = f"error_log_{timestamp}_{random_string}.txt"
 
+    # Create the log file
+    with open(file_name, "w") as log_file:
+        log_file.write(error_message)
+
+    # Upload the log file to the container
+    with open(file_name, "rb") as data:
+        logs_container_client.upload_blob(name=file_name, data=data)
+
+    # Remove the local log file
+    os.remove(file_name)
